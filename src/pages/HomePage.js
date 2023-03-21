3a367b8a8699fc9ed433dddc0bd479d5961a8c7b
sagescholar <syota.kushizaki@gmail.com>
2023-03-22 05:50:53 +0900

    Add homepage, new page, and footer

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import Footer from '../components/Footer';
+
+function HomePage() {
+    return (
+      <div>
+        <h1>Welcome to the homepage</h1>
+        <p>This is the homepage of the website.</p>
+        <p>Feel free to look around and explore!</p>
+        <Footer />
+      </div>
+    );
+  }
+  
+  export default HomePage;
+  
